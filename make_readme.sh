bc4825687e5fdf17a0694af68b711ee98dfedaa1
jobiols <jorge.obiols@gmail.com>
2019-05-21 00:54:48 -0300

    [WIP] para conectar con odoo

---
+++ make_readme.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+oca-gen-addon-readme \
+    --org-name=NTSystemWork \
+    --repo-name=rest_helpdesk_api \
+    --branch=11.0 \
+    --addon-dir=/odoo_ar/odoo-11.0e/click/sources/rest-api/rest_lead_api/ \
+    --gen-html
