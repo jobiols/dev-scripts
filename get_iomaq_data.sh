6561d03462231c222a1f8f5735a1de9e17bc761d
jobiols <jorge.obiols@gmail.com>
2019-09-17 20:42:52 -0300

    [FIX] mejoras

--- get_iomaq_data.sh
+++
@@ -1,3 +0,0 @@
-rm /odoo_ar/odoo-9.0/iomaq/data_dir/product_data/*
-scp ubuntu@iomaq:/odoo_ar/odoo-9.0/iomaq/data_dir/product_data/*.csv /odoo_ar/odoo-9.0/iomaq/data_dir/product_data/
-
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-NEW_DBNAME="AAA"
-sql="SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = ${NEW_DBNAME};"
-echo $sql
-
