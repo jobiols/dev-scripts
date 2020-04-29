6353f93f7645dd15aa59077a99c2d9e7afb49e0e
jobiols <jorge.obiols@gmail.com>
2020-04-29 14:07:09 -0300

    eliminar basura

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,16 @@
+#    -v /odoo_ar/odoo-13.0e/extra-addons:/opt/odoo/extra-addons \
+#    -v /odoo_ar/odoo-13.0e/dist-packages:/usr/lib/python3/dist-packages \
+#    -v /odoo_ar/odoo-13.0e/dist-local-packages:/usr/local/lib/python3.7/dist-local-packages \
+
+
 #!/bin/sh
 sudo docker run --rm -it \
-    -v /odoo_ar/odoo-13.0e/tatakua/config:/opt/odoo/etc/ \
-        -v /odoo_ar/odoo-13.0e/tatakua/data_dir:/opt/odoo/data \
-            -v /odoo_ar/odoo-13.0e/tatakua/log:/var/log/odoo \
-                -v /odoo_ar/odoo-13.0e/tatakua/sources:/opt/odoo/custom-addons \
-                    -v /odoo_ar/odoo-13.0e/tatakua/backup_dir:/var/odoo/backups/ \
-                        -v /odoo_ar/odoo-13.0e/extra-addons:/opt/odoo/extra-addons \
-                            -v /odoo_ar/odoo-13.0e/dist-packages:/usr/lib/python3/dist-packages \
-                                -v /odoo_ar/odoo-13.0e/dist-local-packages:/usr/local/lib/python3.7/dist-local-packages \
-                                    --link pg-tatakua:db \
-                                        -e ODOO_CONF=/dev/null jobiols/odoo-ent:13.0e -- \
-                                            --stop-after-init --logfile=false -d tatakua_test -u all --test-enable
+    -v /odoo_ar/odoo-13.0e/tatakua/config_test:/opt/odoo/etc/ \
+    -v /odoo_ar/odoo-13.0e/tatakua/data_dir:/opt/odoo/data \
+    -v /odoo_ar/odoo-13.0e/tatakua/log:/var/log/odoo \
+    -v /odoo_ar/odoo-13.0e/tatakua/sources_test:/opt/odoo/custom-addons \
+    -v /odoo_ar/odoo-13.0e/tatakua/backup_dir:/var/odoo/backups/ \
+    --link pg-tatakua:db \
+    -e ODOO_CONF=/dev/null \
+    jobiols/odoo-ent:13.0e -- \
+    --stop-after-init --logfile=false -d tatakua_test -u all --test-enable
