a5cfc9b02e4b269f3e518ad1a53ad1bdf9955f91
Jorge Obiols <jorge.obiols@gmail.com>
2021-04-10 15:33:17 -0300

    [FIX] parametro en pgadmin_start

--- pgadmin_start.sh
+++ pgadmin_start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# USO ./pgadmin_start.sh project_name
+
 # Como conectarse con un server odoo
 # Arrancar esto y conectarse a localhost:80
-# Entrar con credenciales odoo / odoo
+# Entrar con credenciales odoo@odoo.com / odoo
 #  General
 #     name nombrecliente
-#  Connection 
+#  Connection
 #	Host nombredelaimagenposgress
 #	Port 5432
 #	Maintenance database: postgres
@@ -16,6 +18,6 @@ sd pull dpage/pgadmin4
 sd run -p 80:80 \
 	-e "PGADMIN_DEFAULT_EMAIL=odoo@odoo.com" \
 	-e "PGADMIN_DEFAULT_PASSWORD=odoo" \
-	--link pg-test13:db \
+	--link pg-$1:db \
 	--name pgadmin4 \
 	-d dpage/pgadmin4
